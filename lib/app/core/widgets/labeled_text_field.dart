023ea99ebd4d33ac2cf3bede00d4f9b14e6901e6
Tony <tony@insoftonline.de>
2026-02-03 01:54:24 +0300

    - create the custom styled text field - add it to the login screen

---
+++ lib/app/core/widgets/labeled_text_field.dart
@@ -0,0 +1,151 @@
+import 'package:flutter/cupertino.dart';
+import 'package:workiom/export.dart';
+
+class LabeledTextField extends StatefulWidget {
+  const LabeledTextField({
+    super.key,
+    required this.label,
+    this.hint = '',
+    required this.controller,
+    this.isPassword = false,
+    this.isNumber = false,
+    this.textAlign = TextAlign.start,
+    this.isCentered = false,
+    this.textDirection,
+    this.primaryColor,
+    this.textNextToLabel,
+    this.validator,
+    this.keyboardType,
+    this.svgSize,
+    this.suffixIcon,
+    this.maxLength = 40,
+    this.focusNode,
+    this.nextFocusNode,
+    this.textInputAction,
+    this.textColor,
+    this.bgColor,
+    this.onFieldSubmitted,
+    this.prefix,
+    this.readOnly = false,
+    this.colorOfEye,
+  });
+
+  final Widget? prefix;
+  final bool isPassword;
+  final bool isNumber;
+  final String label;
+  final String hint;
+  final double? svgSize;
+  final TextAlign textAlign;
+  final TextDirection? textDirection;
+  final bool isCentered;
+  final TextEditingController controller;
+  final Color? primaryColor;
+  final Widget? suffixIcon;
+  final String? textNextToLabel;
+  final TextInputType? keyboardType;
+  final String? Function(String?)? validator;
+  final int maxLength;
+  final FocusNode? focusNode;
+  final FocusNode? nextFocusNode;
+  final TextInputAction? textInputAction;
+  final Function(String)? onFieldSubmitted;
+  final Color? textColor;
+  final Color? bgColor;
+  final bool readOnly;
+  final Color? colorOfEye;
+
+  @override
+  State<LabeledTextField> createState() => _LabeledTextFieldState();
+}
+
+class _LabeledTextFieldState extends State<LabeledTextField> {
+  bool isVisible = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(widget.label, style: Theme.of(context).textTheme.titleMedium),
+
+        Row(
+          children: [
+            if (widget.prefix != null) ...[widget.prefix!, 5.horizontalSpace],
+            Expanded(
+              child: TextFormField(
+                textAlignVertical: widget.isPassword
+                    ? TextAlignVertical.center
+                    : TextAlignVertical.top,
+                controller: widget.controller,
+                cursorColor: TColors.primary,
+                focusNode: widget.focusNode,
+                textInputAction: widget.textInputAction,
+                onFieldSubmitted: (value) {
+                  if (widget.textInputAction == TextInputAction.next) {
+                    FocusScope.of(context).requestFocus(widget.nextFocusNode);
+                  }
+                  widget.onFieldSubmitted?.call(value);
+                },
+                maxLength: widget.maxLength,
+                textAlign: widget.isCentered ? TextAlign.center : widget.textAlign,
+                textDirection: widget.textDirection,
+                validator: widget.validator,
+                keyboardType:
+                    widget.keyboardType ?? (widget.isNumber ? TextInputType.number : null),
+                readOnly: widget.readOnly,
+                inputFormatters: [
+                  if (widget.isNumber) FilteringTextInputFormatter.allow(RegExp(r'^\d+\.?\d{0,2}')),
+                ],
+                decoration: InputDecoration(
+                  filled: false,
+                  hintTextDirection: widget.textDirection,
+                  hintText: widget.hint,
+                  counterText: '',
+                  suffixIcon:
+                      widget.suffixIcon ??
+                      (widget.isPassword
+                          ? IconButton(
+                              onPressed: () {
+                                isVisible = !isVisible;
+                                setState(() {});
+                              },
+                              icon: Icon(
+                                !isVisible ? CupertinoIcons.eye : CupertinoIcons.eye_slash,
+                                color: widget.colorOfEye,
+                              ),
+                            )
+                          : null),
+                  contentPadding: EdgeInsets.symmetric(horizontal: 5.w).copyWith(top: 15.h),
+                  hintStyle: Theme.of(
+                    context,
+                  ).textTheme.bodyMedium!.copyWith(color: TColors.textGrey, fontSize: 13.sp),
+                  errorStyle: Theme.of(
+                    context,
+                  ).textTheme.bodySmall!.copyWith(color: TColors.redColor, fontSize: 12.sp),
+                  errorBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: TColors.redColor),
+                  ),
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: TColors.primary),
+                  ),
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: TColors.greyColor),
+                  ),
+                  focusedErrorBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: TColors.redColor),
+                  ),
+                  disabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: TColors.greyColor),
+                  ),
+                ),
+                obscureText: widget.isPassword && !isVisible,
+                style: Theme.of(context).textTheme.bodyMedium!.copyWith(height: 1.5),
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
--- lib/app/modules/auth/views/login_view.dart
+++ lib/app/modules/auth/views/login_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:workiom/app/core/widgets/app_title.dart';
+import 'package:workiom/app/core/widgets/labeled_text_field.dart';
 
 import 'package:workiom/export.dart';
 
@@ -17,7 +18,30 @@ class LoginView extends GetView {
 
           Expanded(
             child: SingleChildScrollView(
-              child: Column(crossAxisAlignment: .start, children: []),
+              child: Column(
+                crossAxisAlignment: .start,
+                children: [
+                  75.verticalSpace,
+
+                  LabeledTextField(
+                    label: TStrings.yourWorkEmail.tr,
+                    suffixIcon: GestureDetector(
+                      onTap: () {},
+                      child: Icon(CupertinoIcons.clear_circled),
+                    ),
+                    prefix: CustomSvg(Assets.icons.message),
+                    controller: TextEditingController(),
+                  ),
+                  25.verticalSpace,
+                  LabeledTextField(
+                    label: TStrings.yourPassword.tr,
+
+                    isPassword: true,
+                    prefix: CustomSvg(Assets.icons.lock),
+                    controller: TextEditingController(),
+                  ),
+                ],
+              ),
             ),
           ),
         ],
